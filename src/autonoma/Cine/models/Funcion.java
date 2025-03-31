bd615c927d6a448ee2b953570094eda60946ab77
Salomé Carmona G <salome.carmonag@autonoma.edu.co>
2025-03-31 17:50:23 -0500

    Se rellena la clase Funcion

--- src/autonoma/Cine/models/Funcion.java
+++ src/autonoma/Cine/models/Funcion.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package autonoma.Cine.models;
 
-/**
- *
- * @author Estudiante
- */
 public class Funcion {
-    
+     private Pelicula pelicula;
+    private double descuento;
+
+    public Funcion(Pelicula pelicula, double descuento) {
+        this.pelicula = pelicula;
+        this.descuento = descuento;
+    }
+
+    public double getPrecio() {
+        return pelicula.getCostoBase() * (1 - descuento);
+    }
 }
