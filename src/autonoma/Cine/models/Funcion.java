660988af5c07be5b96e7c6cd1051186ce5c8fa77
Salomé Carmona G <salome.carmonag@autonoma.edu.co>
2025-03-31 17:32:21 -0500

    Creacion de main y clase Funcion

---
+++ src/autonoma/Cine/models/Funcion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.Cine.models;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Funcion {
+    
+}
